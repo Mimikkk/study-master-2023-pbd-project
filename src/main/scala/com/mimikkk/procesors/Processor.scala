0d2ce2d5776f114e624289217e066189208b3ff5
Hououin Kyouma <inwazzin@gmail.com>
2023-06-03 23:00:41 +0200

    hate-java: processor

--- src/main/scala/com/mimikkk/procesors/Processor.scala
+++ src/main/scala/com/mimikkk/procesors/Processor.scala
@@ -80,38 +80,38 @@ object Processor {
       .map(intoStockPrice)
       .assignTimestampsAndWatermarks(StockPriceWatermarkStrategy.create())
 
-    recordStream
-      .keyBy(_.stockId)
-      .window(TumblingEventTimeWindows of (Time days 30))
-      .aggregate(new StockPriceRecordAggregator, new StockPriceRecordProcessFunction)
-      .addSink(
-        DatabaseSinkFactory.create[StockPriceRecordProcessFunction.Result](
-          insertStatement,
-          (statement, price) => {
-            statement.setLong(1, price.start)
-            statement.setString(2, price.stockId)
-            statement.setFloat(3, price.close)
-            statement.setFloat(4, price.low)
-            statement.setFloat(5, price.high)
-            statement.setFloat(6, price.volume)
-            statement.setFloat(7, price.close)
-            statement.setFloat(8, price.low)
-            statement.setFloat(9, price.high)
-            statement.setFloat(10, price.volume)
-          },
-          configuration.database.url,
-          configuration.database.username,
-          configuration.database.password
-        )
-      )
-
 //    recordStream
 //      .keyBy(_.stockId)
-//      .window(TumblingEventTimeWindows of (Time days configuration.anomaly.dayRange))
-//      .aggregate(new StockPriceAnomalyAggregator, new StockPriceAnomalyProcessFunction)
-//      .filter(result => result.fluctuation > configuration.anomaly.percentageFluctuation)
-//      .map(_.toString)
-//      .sinkTo(KafkaSinkFactory.create(configuration.kafka.server, configuration.kafka.anomalyTopic))
+//      .window(TumblingEventTimeWindows of (Time days 30))
+//      .aggregate(new StockPriceRecordAggregator, new StockPriceRecordProcessFunction)
+//      .addSink(
+//        DatabaseSinkFactory.create[StockPriceRecordProcessFunction.Result](
+//          insertStatement,
+//          (statement, price) => {
+//            statement.setLong(1, price.start)
+//            statement.setString(2, price.stockId)
+//            statement.setFloat(3, price.close)
+//            statement.setFloat(4, price.low)
+//            statement.setFloat(5, price.high)
+//            statement.setFloat(6, price.volume)
+//            statement.setFloat(7, price.close)
+//            statement.setFloat(8, price.low)
+//            statement.setFloat(9, price.high)
+//            statement.setFloat(10, price.volume)
+//          },
+//          configuration.database.url,
+//          configuration.database.username,
+//          configuration.database.password
+//        )
+//      )
+
+    recordStream
+      .keyBy(_.stockId)
+      .window(TumblingEventTimeWindows of (Time days configuration.anomaly.dayRange))
+      .aggregate(new StockPriceAnomalyAggregator, new StockPriceAnomalyProcessFunction)
+      .filter(result => result.fluctuation > configuration.anomaly.percentageFluctuation)
+      .map(_.toString)
+      .sinkTo(KafkaSinkFactory.create(configuration.kafka.server, configuration.kafka.anomalyTopic))
 
     environment.execute("Stock prices processing...")
   }
