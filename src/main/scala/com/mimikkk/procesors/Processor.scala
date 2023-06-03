c60bc44e58028948c098a2b326b1c44e618f60c9
Hououin Kyouma <inwazzin@gmail.com>
2023-06-04 01:17:14 +0200

    hate-java: processor KURWA

--- src/main/scala/com/mimikkk/procesors/Processor.scala
+++ src/main/scala/com/mimikkk/procesors/Processor.scala
@@ -52,22 +52,20 @@ object Processor {
       System.exit(1)
     }
 
-    val meta: String = args(0)
+    val meta = args(0)
+    val server = args(1)
+    val groupId = args(2)
+    val contentTopic = args(3)
+    val anomalyTopic = args(4)
+    val url = args(5)
+    val username = args(6)
+    val password = args(7)
 
-    val server: String = args(1)
-    val groupId: String = args(2)
-    val contentTopic: String = args(3)
-    val anomalyTopic: String = args(4)
+    val dayRange = args(8).toInt
+    val percentageFluctuation = args(9).toFloat / 100
+    val updateStrategy = UpdateStrategy.from(args(10))
 
-    val url: String = args(5)
-    val username: String = args(6)
-    val password: String = args(7)
-
-    val dayRange: Int = args(8).toInt
-    val percentageFluctuation: Float = args(9).toFloat / 100
-    val updateStrategy: UpdateStrategy.Type = UpdateStrategy.from(args(10))
-
-    val insertStatement: String =
+    val insertStatement =
       """
       INSERT INTO stock_prices (
         window_start,
@@ -104,31 +102,31 @@ object Processor {
       .map(intoStockPrice)
       .assignTimestampsAndWatermarks(StockPriceWatermarkStrategy.create())
 
-    recordStream
-      .keyBy(_.stockId)
-      .window(TumblingEventTimeWindows of (Time days 30))
-      .aggregate(new StockPriceRecordAggregator, new StockPriceRecordProcessFunction)
-      .addSink(DatabaseSinkFactory.create[StockPriceRecordProcessFunction.Result](
-        insertStatement,
-        // Has to be verbose to ensure serialization for Spark preprocessor
-        new JdbcStatementBuilder[StockPriceRecordProcessFunction.Result] {
-          override def accept(statement: PreparedStatement, price: StockPriceRecordProcessFunction.Result): Unit = {
-            statement.setLong(1, price.start)
-            statement.setString(2, price.stockId)
-            statement.setFloat(3, price.close)
-            statement.setFloat(4, price.low)
-            statement.setFloat(5, price.high)
-            statement.setFloat(6, price.volume)
-            statement.setFloat(7, price.close)
-            statement.setFloat(8, price.low)
-            statement.setFloat(9, price.high)
-            statement.setFloat(10, price.volume)
-          }
-        },
-        url,
-        username,
-        password
-      ))
+//    recordStream
+//      .keyBy(_.stockId)
+//      .window(TumblingEventTimeWindows of (Time days 30))
+//      .aggregate(new StockPriceRecordAggregator, new StockPriceRecordProcessFunction)
+//      .addSink(DatabaseSinkFactory.create[StockPriceRecordProcessFunction.Result](
+//        insertStatement,
+//        // Has to be verbose to ensure serialization for Spark preprocessor
+//        new JdbcStatementBuilder[StockPriceRecordProcessFunction.Result] {
+//          override def accept(statement: PreparedStatement, price: StockPriceRecordProcessFunction.Result): Unit = {
+//            statement.setLong(1, price.start)
+//            statement.setString(2, price.stockId)
+//            statement.setFloat(3, price.close)
+//            statement.setFloat(4, price.low)
+//            statement.setFloat(5, price.high)
+//            statement.setFloat(6, price.volume)
+//            statement.setFloat(7, price.close)
+//            statement.setFloat(8, price.low)
+//            statement.setFloat(9, price.high)
+//            statement.setFloat(10, price.volume)
+//          }
+//        },
+//        url,
+//        username,
+//        password
+//      ))
 
     recordStream
       .keyBy(_.stockId)
