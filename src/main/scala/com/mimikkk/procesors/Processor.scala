2762759aa53aa272b14da07ea58c220faa175c76
Hououin Kyouma <inwazzin@gmail.com>
2023-06-03 23:21:45 +0200

    hate-java: processor

--- src/main/scala/com/mimikkk/procesors/Processor.scala
+++ src/main/scala/com/mimikkk/procesors/Processor.scala
@@ -110,7 +110,7 @@ object Processor {
       .window(TumblingEventTimeWindows of (Time days configuration.anomaly.dayRange))
       .aggregate(new StockPriceAnomalyAggregator, new StockPriceAnomalyProcessFunction)
       .filter(result => result.fluctuation > configuration.anomaly.percentageFluctuation)
-      .map(_.toString)
+//      .map(_.toString)
 //      .sinkTo(KafkaSinkFactory.create(configuration.kafka.server, configuration.kafka.anomalyTopic))
 
 //    environment.execute("Stock prices processing...")
