4a283f64ef63dba0df5e42d9e2ac5b264f239500
Hououin Kyouma <inwazzin@gmail.com>
2023-06-04 15:57:35 +0200

    i-hate-java: processor

--- src/main/scala/com/mimikkk/procesors/KafkaProcessor.scala
+++ src/main/scala/com/mimikkk/procesors/KafkaProcessor.scala
@@ -131,28 +131,29 @@ object KafkaProcessor extends Processor {
     .trigger(ContinuousProcessingTimeTrigger of every)
     .aggregate(new StockPriceRecordAggregator, new StockPriceRecordProcessFunction)
     .map(new StockPriceRecordEntitleFunction)
-    .addSink(DatabaseSinkFactory.create[StockPriceRecordEntitleFunction.Result](
-      insertStatement,
-      // Has to be verbose to ensure serialization for Spark preprocessor
-      new JdbcStatementBuilder[StockPriceRecordEntitleFunction.Result] {
-        override def accept(statement: PreparedStatement, price: StockPriceRecordEntitleFunction.Result): Unit = {
-          statement.setLong(1, price.start)
-          statement.setString(2, price.stockId)
-          statement.setString(3, price.title)
-          statement.setFloat(4, price.close)
-          statement.setFloat(5, price.low)
-          statement.setFloat(6, price.high)
-          statement.setFloat(7, price.volume)
-          statement.setFloat(8, price.close)
-          statement.setFloat(9, price.low)
-          statement.setFloat(10, price.high)
-          statement.setFloat(11, price.volume)
-        }
-      },
-      url,
-      username,
-      password
-    ))
+    .print()
+//    .addSink(DatabaseSinkFactory.create[StockPriceRecordEntitleFunction.Result](
+//      insertStatement,
+//      // Has to be verbose to ensure serialization for Spark preprocessor
+//      new JdbcStatementBuilder[StockPriceRecordEntitleFunction.Result] {
+//        override def accept(statement: PreparedStatement, price: StockPriceRecordEntitleFunction.Result): Unit = {
+//          statement.setLong(1, price.start)
+//          statement.setString(2, price.stockId)
+//          statement.setString(3, price.title)
+//          statement.setFloat(4, price.close)
+//          statement.setFloat(5, price.low)
+//          statement.setFloat(6, price.high)
+//          statement.setFloat(7, price.volume)
+//          statement.setFloat(8, price.close)
+//          statement.setFloat(9, price.low)
+//          statement.setFloat(10, price.high)
+//          statement.setFloat(11, price.volume)
+//        }
+//      },
+//      url,
+//      username,
+//      password
+//    ))
 
 
   private final val percentageFluctuation = configuration.anomaly.percentageFluctuation
@@ -163,7 +164,8 @@ object KafkaProcessor extends Processor {
     .map(new StockPriceAnomalyEntitleFunction)
     .filter(_.fluctuation > percentageFluctuation)
     .map(_.toString)
-    .sinkTo(KafkaSinkFactory.create(configuration.kafka.server, configuration.kafka.anomalyTopic))
+    .print()
+//    .sinkTo(KafkaSinkFactory.create(configuration.kafka.server, configuration.kafka.anomalyTopic))
 
   environment.execute("Stock prices processing...")
 }
