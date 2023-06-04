32f8382525ba5a64bf34e3180febb5ad05692a52
Hououin Kyouma <inwazzin@gmail.com>
2023-06-04 17:57:01 +0200

    fuck you

--- src/main/scala/com/mimikkk/models/stockprice/StockPriceWatermarkStrategy.scala
+++ src/main/scala/com/mimikkk/models/stockprice/StockPriceWatermarkStrategy.scala
@@ -1,30 +1,28 @@
 package com.mimikkk.models.stockprice
 
 
-import org.apache.flink.api.common.eventtime.{TimestampAssignerSupplier, Watermark, WatermarkGeneratorSupplier, WatermarkOutput, TimestampAssigner, WatermarkGenerator, WatermarkStrategy}
+import org.apache.flink.api.common.eventtime.{TimestampAssignerSupplier, Watermark, WatermarkGeneratorSupplier, WatermarkOutput, TimestampAssigner => TimestampAssignerBase, WatermarkGenerator => WatermarkGeneratorBase, WatermarkStrategy => WatermarkStrategyBase}
 
 
-final class StockPriceWatermarkStrategy extends WatermarkStrategy[StockPrice] {
-  override def createWatermarkGenerator(context: WatermarkGeneratorSupplier.Context) = new StockPriceWatermarkGenerator()
+final class StockPriceWatermarkStrategy extends WatermarkStrategyBase[StockPrice] {
+  override def createWatermarkGenerator(context: WatermarkGeneratorSupplier.Context) = new WatermarkGenerator()
 
-  override def createTimestampAssigner(context: TimestampAssignerSupplier.Context) = new StockPriceTimestampAssigner()
+  override def createTimestampAssigner(context: TimestampAssignerSupplier.Context) = new TimestampAssigner()
 
-}
+  final class WatermarkGenerator extends WatermarkGeneratorBase[StockPrice] {
+    private val maxOutOfOrderSeconds = 24 * 60 * 60 * 1000 // 24 hours
+    private var maxTimestamp: Long = _
 
-final class StockPriceWatermarkGenerator extends WatermarkGenerator[StockPrice] {
-  private val maxOutOfOrderness: Long = 24 * 60 * 60 * 1000
+    override def onEvent(item: StockPrice, long: Long, output: WatermarkOutput): Unit = {
+      maxTimestamp = math.max(item.date.getTime, maxTimestamp)
+    }
 
-  private var currentMaxTimestamp: Long = 1
-
-  override def onEvent(t: StockPrice, l: Long, watermarkOutput: WatermarkOutput): Unit = {
-    currentMaxTimestamp = math.max(0, currentMaxTimestamp)
+    override def onPeriodicEmit(watermarkOutput: WatermarkOutput): Unit = {
+      watermarkOutput.emitWatermark(new Watermark(maxTimestamp - maxOutOfOrderSeconds - 1))
+    }
   }
 
-  override def onPeriodicEmit(watermarkOutput: WatermarkOutput): Unit = {
-    watermarkOutput.emitWatermark(new Watermark(currentMaxTimestamp - maxOutOfOrderness - 1))
+  final class TimestampAssigner extends TimestampAssignerBase[StockPrice] {
+    override def extractTimestamp(model: StockPrice, long: Long): Long = model.date.getTime
   }
-}
-
-final class StockPriceTimestampAssigner extends TimestampAssigner[StockPrice] {
-  override def extractTimestamp(t: StockPrice, l: Long): Long = 0
 }
