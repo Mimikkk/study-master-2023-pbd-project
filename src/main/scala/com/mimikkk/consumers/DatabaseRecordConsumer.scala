e37e6bf9e8fc495b6c18ac7e32c0660101962e05
Hououin Kyouma <inwazzin@gmail.com>
2023-06-04 02:26:34 +0200

    Revert "KURWEA"
    This reverts commit e447bb9e9e49983cbfc967ee9b13b237509ad45a.

---
+++ src/main/scala/com/mimikkk/consumers/DatabaseRecordConsumer.scala
@@ -0,0 +1,50 @@
+package com.mimikkk.consumers
+
+import java.sql.{Connection, Date, DriverManager}
+
+object DatabaseRecordConsumer extends RecordConsumer {
+  if (args.length != 3) {
+    println(
+      """
+        |Usage of DatabaseRecordConsumer:
+        |  <database_url: url-string>
+        |  <username: string>
+        |  <password: string>
+      """.stripMargin
+    )
+    System.exit(1)
+  }
+  private final val url = args(0)
+  private final val username = args(1)
+  private final val password = args(2)
+  private final val selectRecords = "SELECT * FROM stock_prices ORDER BY window_start DESC LIMIT 50"
+
+  private final var connection: Connection = _
+  try {
+    connection = DriverManager.getConnection(url, username, password)
+    val statement = connection.createStatement
+
+    while (true) {
+      val records = statement executeQuery selectRecords
+
+      println("< Stream Found >")
+      while (records.next) {
+        val start = new Date(records getLong "window_start").toLocalDate
+        val end = start plusDays 30
+
+        val stockPriceId = records getString "id"
+        val stock = records getString "stock"
+
+        val from = s"${start.getYear}-${start.getMonth}-${start.getDayOfMonth}"
+        val to = s"${end.getYear}-${end.getMonth}-${end.getDayOfMonth}"
+
+        println(s"$from - $to \t $stock($stockPriceId)")
+      }
+
+      Thread.sleep(1000)
+    }
+  } catch {
+    case e: Exception => e printStackTrace()
+  }
+  connection close()
+}
---
+++ src/main/scala/com/mimikkk/consumers/RecordConsumer.scala
@@ -0,0 +1,3 @@
+package com.mimikkk.consumers
+
+trait RecordConsumer extends App {}
