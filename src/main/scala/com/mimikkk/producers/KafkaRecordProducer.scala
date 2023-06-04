5ffae323fc98a1d45aecd63f63c224d4c05b0e54
Hououin Kyouma <inwazzin@gmail.com>
2023-06-04 15:19:05 +0200

    i-hate-java: kafka producer

--- src/main/scala/com/mimikkk/producers/KafkaRecordProducer.scala
+++ src/main/scala/com/mimikkk/producers/KafkaRecordProducer.scala
@@ -23,37 +23,35 @@ object KafkaRecordProducer extends RecordProducer {
     System.exit(1)
   }
 
-  private final object configuration {
-    final val partsDirectory = args(0)
-    final val secondsBetweenParts = args(1).toInt
-    final val topic = args(2)
-    final val server = args(3)
-  }
+  final val partsDirectory = args(0)
+  final val secondsBetweenParts = args(1).toInt
+  final val topic = args(2)
+  final val server = args(3)
 
   private final val properties = new Properties {
     putAll(Map(
-      "bootstrap.servers" -> configuration.server,
+      "bootstrap.servers" -> server,
       "key.serializer" -> "org.apache.kafka.common.serialization.StringSerializer",
       "value.serializer" -> "org.apache.kafka.common.serialization.StringSerializer"
     ).asJava)
   }
 
   private final val producer = new KafkaProducer[String, String](properties)
-  private final val partsPaths = new File(configuration.partsDirectory) listFiles() map (_.getAbsolutePath)
+  private final val partsPaths = new File(partsDirectory) listFiles() map (_.getAbsolutePath)
 
   println("Start broadcasting...")
   partsPaths.sorted.zipWithIndex foreach (pair => try {
     val (path, index) = pair
 
-    println(s"< Stream $path : '${index + 1}/${partsPaths.length}' Sending... >")
+    println(s"< Stream '${index + 1}/${partsPaths.length}' Sending... >")
     lines(Paths get path) skip 1 forEach (
       row => {
         println("  " + row)
-        producer send new ProducerRecord(configuration.topic, row.split(',')(0), row)
+        producer send new ProducerRecord(topic, row.split(',')(0), row)
       }
       )
     println(s"< Stream Sent. >")
-    SECONDS sleep configuration.secondsBetweenParts
+    SECONDS sleep secondsBetweenParts
   } catch {
     case e: Throwable => e printStackTrace()
   })
